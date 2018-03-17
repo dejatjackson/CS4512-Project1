c0d6b34fd81066c0369d06ca1b74e28deaa1b585
Sean Hale <seanhale1@live.com>
2018-03-17 18:56:42 -0400

    cp cleanup

--- mycommands.cpp
+++ mycommands.cpp
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h> // For exit()
 #include <string.h>
 #include "mycommands.h"
 
@@ -14,28 +13,24 @@ int clear() {
 int cp(char *args[]) {
     FILE *fptr1, *fptr2;
     char c;
-    int lengthOfFile;
 
     // Open first file for reading
     fptr1 = fopen(args[1], "r");
-    if (fptr1 == NULL)
-    {
-        printf("Cannot open file %s \n", args[1]);
-        exit(0);
+    if (fptr1 == NULL) {
+        printf("Cannot find file %s \n", args[1]);
+        return 1;
     }
 
     // Opens other file for writing
     fptr2 = fopen(args[2], "w");
-    if (fptr2 == NULL)
-    {
+    if (fptr2 == NULL) {
         printf("Cannot open file %s \n", args[2]);
-        exit(0);
+        return 1;
     }
 
     // Read contents from file
     c = fgetc(fptr1);
-    while (c != EOF)
-    {
+    while (c != EOF) {
         fputc(c, fptr2);
         c = fgetc(fptr1);
     }
